215fbeaef4473bb4ed3f0a83c47edf1e0da50ab9
Lina <107427736+Lina-cpp@users.noreply.github.com>
2022-09-10 12:38:15 +0200

    Initial commit

---
+++ Source/Shooter/Projectile.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh")); //creating mesh to choose in BP
+	RootComponent = ProjectileMesh; //attaching ProjectileMesh to Root
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	ProjectileMovementComponent->MaxSpeed = 1300.f; 	//projectile max speed
+	ProjectileMovementComponent->InitialSpeed = 1300.f; //projectile start speed
+	ProjectileMovementComponent->ProjectileGravityScale = 0; //0 gravity
+
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
